10e0c18f4cc8500a197acdd6d9b6dd4d4b6a1550
Lena <elena.pokhvalennaya@gmail.com>
2022-04-21 19:50:25 +0300

    update task

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 import storage from "./storage.js";
+
 fetch("https://jsonplaceholder.typicode.com/posts/1")
   .then((response) => response.json())
   .then((data) => {
-    storage.set("data", data);
+    if (storage.get("data" !== null)) {
+      console.log(data);
+    } else {
+      storage.set("data", data);
+    }
   })
   .catch((error) => console.error(error));
 
 function isValidDateFormat(str) {
-  let regexp = /^\d\d\d\d\/\d\d\/\d\d \d\d:\d\d$/;
+  let regexp = /^\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}$/;
   return regexp.test(str);
 }
 
-console.log(isValidDateFormat("12.02.2022 12:10"));
+console.log(isValidDateFormat("2012/09/18 12:10"));
